4c84360b9251a50cfc4f87271ec46417c1a1c82c
Ben Davis <ben.davis@ruralsourcing.com>
2021-12-02 12:02:31 -0600

    add windowed count

--- AdventOfCode/Library.fs
+++ AdventOfCode/Library.fs
@@ -1,11 +1,20 @@
 ﻿namespace AdventOfCode
 
 module DepthMeasurement =
+    let checkForIncrease (x,y) = if y > x then 1 else 0
+
     let count depths =
-        let checkForIncrease (x,y) =
-            if y > x then 1 else 0
         depths
         |> List.pairwise
         |> List.map checkForIncrease
         |> List.sum
+
+    let windowSum (depths: int List) =
+        depths |> List.sum
+
+    let windowCount depths =
+        depths
+        |> List.windowed 3 
+        |> List.map windowSum
+        |> count
 
