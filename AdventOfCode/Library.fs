397bd93cfc51b3d03682bca8116d1113cfc984ad
Ben Davis <ben.davis@ruralsourcing.com>
2021-12-02 11:30:59 -0600

    Add project files.

---
+++ AdventOfCode/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode
+
+module DepthMeasurement =
+    let count depths =
+        let checkForIncrease (x,y) =
+            if y > x then 1 else 0
+        depths
+        |> List.pairwise
+        |> List.map checkForIncrease
+        |> List.sum
+
---
+++ Test/Day01.fs
@@ -0,0 +1,25 @@
+﻿module Day01
+
+open System
+open Xunit
+open AdventOfCode
+
+[<Fact>]
+let ``Sonar sweep counts increases``() =
+    let depthList = 
+        [
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+        ]
+    let result = DepthMeasurement.count depthList
+
+    Assert.Equal(7, result)
+
