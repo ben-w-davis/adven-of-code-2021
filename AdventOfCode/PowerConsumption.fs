00b75b0d25fe915fe4fa9cd8c76037d88ad74f7a
Ben Davis <ben.davis@ruralsourcing.com>
2021-12-03 13:19:35 -0600

    single gama report list

--- AdventOfCode/PowerConsumption.fs
+++ AdventOfCode/PowerConsumption.fs
@@ -4,6 +4,14 @@ type Bit =
     | One 
     | Zero
 
+type BitAccumulator = {
+    Ones : int
+    Zeros: int }
+
+let init = { Ones = 0; Zeros = 0; }
+let initAll (reports : Bit List List) =
+    let head = reports |> List.head
+    head |> List.map (fun x -> init)
 let decodeOneBit bit =
     match bit with
     | '1' -> One
@@ -17,11 +25,32 @@ let decodeBits sample =
         |> Seq.toList
     temp
 
+let calculate acc bit =
+    match bit with
+    | One -> { acc with Ones = acc.Ones + 1 }
+    | Zero -> { acc with Zeros = acc.Zeros + 1 }
+
+let calculateAll (accList : BitAccumulator list) (report : Bit List) =
+    report
+    |> List.zip accList
+    |> List.map (fun (acc, bit) -> calculate acc bit)
+
+let choose acc = 
+    if acc.Ones > acc.Zeros then One else Zero
+
+let chooseAll accList = 
+    accList |> List.map choose
+
 let gammaBit bitList =
-    let count = bitList |> List.where (fun x -> x = One) |> List.length
-    if count > (bitList.Length / 2) then
-        One
-    else
-        Zero
+    bitList
+    |> List.fold calculate init
+    |> choose
+
+let gamma (reports : Bit List List) =
+    reports
+    |> List.fold calculateAll (initAll reports )
+    |> chooseAll
+    
+    //[Zero;Zero;One;Zero;Zero]
     
 
--- Test/Day03.fs
+++ Test/Day03.fs
@@ -42,12 +42,29 @@ let ``Gamma rate of first bit``() =
 
     Assert.Equal(One, result)
 
+
+let compareBitList (expected : Bit List) (result : Bit list) =
+    expected
+    |> List.zip result
+    |> List.iter (fun (e,r) -> Assert.Equal(e,r))
+
 [<Fact>]
 let ``Convert string bit list to Bits``() =
     let sample = "00100"
     let result = decodeBits sample
     let expected = [Zero;Zero;One;Zero;Zero]
 
-    expected
-    |> List.zip result
-    |> List.iter (fun (e,r) -> Assert.Equal(e,r))
+    compareBitList expected result
+
+[<Fact>]
+let ``Get gamma for a single report list``() =
+    let sample = 
+        [
+            [Zero;Zero;One;Zero;Zero]
+        ]
+
+    let result = gamma sample
+    let expected = [Zero;Zero;One;Zero;Zero]
+
+    compareBitList expected result
+
