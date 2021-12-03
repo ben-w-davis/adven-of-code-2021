ddd548ce7ef8ffff3842be7ca05870f18e406535
Ben Davis <ben.davis@ruralsourcing.com>
2021-12-03 11:34:34 -0600

    decode power consumption diagnostic report to bits

---
+++ AdventOfCode/PowerConsumption.fs
@@ -0,0 +1,27 @@
+﻿module PowerConsumption
+
+type Bit =
+    | One 
+    | Zero
+
+let decodeOneBit bit =
+    match bit with
+    | '1' -> One
+    | '0' -> Zero
+    | _ -> failwith "Invalid bit"
+
+let decodeBits sample =
+    let temp =
+        sample
+        |> Seq.map decodeOneBit
+        |> Seq.toList
+    temp
+
+let gammaBit bitList =
+    let count = bitList |> List.where (fun x -> x = One) |> List.length
+    if count > (bitList.Length / 2) then
+        One
+    else
+        Zero
+    
+
---
+++ Test/Day03.fs
@@ -0,0 +1,54 @@
+﻿module Day03
+
+open System
+open Xunit
+open PowerConsumption
+
+let sampleInput =
+    [
+        "00100"
+        "11110"
+        "10110"
+        "10111"
+        "10101"
+        "01111"
+        "00111"
+        "11100"
+        "10000"
+        "11001"
+        "00010"
+        "01010"
+    ]
+
+[<Fact>]
+let ``Gamma rate of first bit``() =
+    let sample = 
+        [
+            Zero
+            One
+            One
+            One
+            One
+            Zero
+            Zero
+            One
+            One
+            One
+            Zero
+            Zero
+        ]
+
+    let result = gammaBit sample
+
+    Assert.Equal(One, result)
+
+[<Fact>]
+let ``Convert string bit list to Bits``() =
+    let sample = "00100"
+    let result : Bit list = decodeBits sample
+
+    let expected : Bit list = [Zero;Zero;One;Zero;Zero]
+
+    expected
+    |> List.zip result
+    |> List.iter (fun (e,r) -> Assert.Equal(e,r))
