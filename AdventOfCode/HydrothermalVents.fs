b9e10b25e36f48230401db2e736f0adb0588facf
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-06 17:17:39 -0600

    begin Day 5, parse line

---
+++ AdventOfCode/HydrothermalVents.fs
@@ -0,0 +1,27 @@
+﻿module HydrothermalVents
+open System
+
+type Line = {
+    x1 : int
+    y1 : int
+    x2 : int
+    y2 : int
+}
+
+let toInt (num:string) =
+    match Int32.TryParse num with
+    | (true, n) -> n
+    | _ -> failwith "No number"
+
+let parseLine (line:string) =
+    let parts = line.Split(' ')
+    let firstPair = parts.[0].Split(',')
+    let secondPair = parts.[2].Split(',')
+    { 
+        x1 = firstPair.[0] |> toInt
+        y1 = firstPair.[1] |> toInt
+        x2 = secondPair.[0] |> toInt
+        y2 = secondPair.[1] |> toInt
+    }
+
+
---
+++ Test/Day05.fs
@@ -0,0 +1,30 @@
+﻿module Day05
+open System
+open Xunit
+open HydrothermalVents
+
+let sampleInput =
+    """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"""
+
+[<Fact>]
+let ``Decode input first line``() =
+    let line = "0,9 -> 5,9"
+    let result = parseLine line
+
+    Assert.Equal(0, result.x1)
+    Assert.Equal(9, result.y1)
+    Assert.Equal(5, result.x2)
+    Assert.Equal(9, result.y2)
+[<Fact>]
+let ``Keep only horizontal and vertical lines``() =
+    ()
+     
