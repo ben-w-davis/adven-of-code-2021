c346276911556db0356ae9b180b626321730bcb7
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-07 12:55:24 -0600

    day 6 - lantern fish tick

---
+++ AdventOfCode/Lanternfish.fs
@@ -0,0 +1,22 @@
+﻿module Lanternfish
+
+type Lanternfish = {
+    Timer : int
+}
+
+let createLanternfish internalTimer =
+    {
+        Timer = internalTimer
+    }
+
+let timerTick lanternfish =
+    match lanternfish.Timer with
+    | 0 -> [ createLanternfish 6; createLanternfish 8 ]
+    |_ ->
+        [
+            { lanternfish with Timer = lanternfish.Timer - 1 }
+        ]
+
+let timerTickAll fishes =
+    fishes
+    |> List.collect timerTick
--- Test/Day06.fs
+++ Test/Day06.fs
@@ -1,13 +1,49 @@
 ﻿module Day06
 open System
 open Xunit
-open Laternfish
+open Lanternfish
 
 let sampleInput = "3,4,3,1,2"
 
 [<Fact>]
-let ``Create laternfish from internal timer state``() =
+let ``Create lanternfish from internal timer state``() =
     let internalTimer = 3
-    let result = createLaternfish internalTimer
+    let result = createLanternfish internalTimer
 
     Assert.Equal(3, result.Timer)
+
+[<Fact>]
+let ``Lanternfish ticks down by day``() =
+    let fish = createLanternfish 3
+    let result = timerTick fish
+
+    let expected = [|createLanternfish 2|]
+    Assert.Equal(expected, result)
+
+[<Fact>]
+let ``Lanternfish ticks down by day to create new fish``() =
+    let fish = createLanternfish 0
+    let result = timerTick fish
+
+    let expected = 
+        [|
+            createLanternfish 6
+            createLanternfish 8
+        |]
+    Assert.Equal(expected, result)
+
+
+[<Fact>]
+let ``Timer tick down across multiple fish``() =
+    let fishes =
+        [
+            createLanternfish 1
+            createLanternfish 3
+        ]
+    let  result = timerTickAll fishes
+    let expected =
+        [|
+            createLanternfish 0
+            createLanternfish 2
+        |]
+    Assert.Equal(expected, result)
