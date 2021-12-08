d9c06ced21c48ff095c8d9b01c52b9af7c21915b
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-08 17:53:28 -0600

    many crabs fuel usage for a position

---
+++ AdventOfCode/CrabSubmarines.fs
@@ -0,0 +1,12 @@
+﻿module CrabSubmarines
+open System
+
+let fuelUse (crab:int) (position:int) =
+    let result = crab - position
+    if result < 0 then -1 * result else result
+
+let allFuelUse crabs position =
+    crabs
+    |> List.map (fuelUse position)
+    |> List.sum
+
---
+++ Test/Day07.fs
@@ -0,0 +1,24 @@
+﻿module Day07
+open System
+open Xunit
+open CrabSubmarines
+
+let sampleInput = "16,1,2,0,4,2,7,1,2,14"
+
+[<Theory>]
+[<InlineData(16,2,14)>]
+[<InlineData(1,2,1)>]
+[<InlineData(2,2,0)>]
+[<InlineData(0,2,2)>]
+let ``Calculate fuel usage for one crab``(crab, position, expected) =
+    let result = fuelUse crab position
+
+    Assert.Equal(expected, result)
+
+[<Fact>]
+let ``Calculate fuel usage for many crabs``() =
+    let crabs = [16;1;2;0;4;2;7;1;2;14]
+    let position = 2
+    let result = allFuelUse crabs position
+
+    Assert.Equal(37, result)
