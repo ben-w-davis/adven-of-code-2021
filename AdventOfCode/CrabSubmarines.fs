671ed4d83d42feda8247e536e0436c1cbef540a0
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-09 11:38:29 -0600

    finish day 7

--- AdventOfCode/CrabSubmarines.fs
+++ AdventOfCode/CrabSubmarines.fs
@@ -5,9 +5,26 @@ let fuelUse (crab:int) (position:int) =
     let result = crab - position
     if result < 0 then -1 * result else result
 
+let fuelTable =
+    [1 .. 2000]
+    |> List.map (fun n -> (n, [1..n] |> List. sum))
+    |> dict
+
+let betterFuelUse (crab:int) (position:int) =
+    let temp = fuelTable
+    let rawFuel = fuelUse crab position
+    match temp.TryGetValue rawFuel with
+    | (true, value) -> value
+    | _ -> [1 .. rawFuel] |> List.sum
+
 let allFuelUse crabs position =
     crabs
     |> List.map (fuelUse position)
+    |> List.sum
+
+let bestFuelUse crabs position =
+    crabs
+    |> List.map (betterFuelUse position)
     |> List.sum
 
 let getAveragePosition crabs = 
@@ -27,4 +44,19 @@ let getMostCommonPosition (crabs:int list) =
     |> List.head
     |> fst
 
+let getLowestFuelUsage crabs =
+    let lowest = getLowestPosition crabs
+    let highest = getHighestPosition crabs
 
+    [lowest .. highest]
+    |> List.map (fun x -> (allFuelUse crabs x, x))
+    |> List.minBy fst
+    |> fst
+        
+let getBestFuelUsage crabs =
+    let lowest = getLowestPosition crabs
+    let highest = getHighestPosition crabs
+    [lowest .. highest]
+    |> List.map (fun x -> (bestFuelUse crabs x, x))
+    |> List.minBy fst
+    |> fst
