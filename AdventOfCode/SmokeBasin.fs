5d246b1515a9a1748883d389b3ba2abd6ced00ac
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-12 15:59:08 -0600

    use fsunit, read sample input

--- AdventOfCode/SmokeBasin.fs
+++ AdventOfCode/SmokeBasin.fs
@@ -1,2 +1,12 @@
 ﻿module SmokeBasin
+open System
+
+let readInput (input:string) =
+    let splitLine (str:string) =
+        str
+        |> Seq.map (Char.GetNumericValue >> int)
+        |> Seq.toArray
+    input.Split("\r\n")
+    |> Array.map splitLine
+
 
--- Test/Day09.fs
+++ Test/Day09.fs
@@ -1,5 +1,18 @@
 ﻿module Day09
 open System
 open Xunit
+open FsUnit.Xunit
 open SmokeBasin
 
+let sampleInput = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
+
+[<Fact>]
+let ``Read input into array``() =
+    let result = readInput sampleInput
+    let expected = [|2;1;9;9;9;4;3;2;1;0|]
+
+    result[0] |> should equal expected
