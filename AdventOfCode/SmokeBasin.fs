b7aec0e7f65bfbe056f12878ef4a69432e5801ef
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-12 16:29:08 -0600

    get values out of heightmap

--- AdventOfCode/SmokeBasin.fs
+++ AdventOfCode/SmokeBasin.fs
@@ -9,4 +9,32 @@ let readInput (input:string) =
     input.Split("\r\n")
     |> Array.map splitLine
 
+let getValue (heightmap:int[][]) (x,y) =
+    heightmap[y][x]
+
+let getNeighbors (heightmap : int[][]) (x,y) =
+    let top = 
+        if y > 0 then
+            getValue heightmap (x,y-1) |> Some
+        else
+            None
+    let bottom = 
+        if y < heightmap.Length - 1 then
+            getValue heightmap (x,y+1) |> Some
+        else
+            None
+
+    let left = 
+        if x > 0 then
+            getValue heightmap (x-1,y) |> Some
+        else
+            None
+    let right = 
+        if x < heightmap[0].Length - 1 then
+            getValue heightmap (x+1,y) |> Some
+        else
+            None
+
+    [left; right; top; bottom] |> List.choose id
+
 
--- Test/Day09.fs
+++ Test/Day09.fs
@@ -11,8 +11,62 @@ let sampleInput = """2199943210
 9899965678"""
 
 [<Fact>]
-let ``Read input into array``() =
+let ``Read input into heightmap``() =
     let result = readInput sampleInput
     let expected = [|2;1;9;9;9;4;3;2;1;0|]
 
     result[0] |> should equal expected
+
+[<Fact>]
+let ``We can index into middle of heightmap``() =
+    let result = readInput sampleInput
+
+    result[1][1] |> should equal 9
+
+[<Fact>]
+let ``Get neighbors for item in middle of heightmap``() =
+    let heightmap = readInput sampleInput
+    let result = getNeighbors heightmap (1,1)
+
+    let expected = [3;8;1;8]
+    result |> should equal expected
+
+[<Fact>]
+let ``Get value from coordinates``() =
+    let heightmap = readInput sampleInput
+    let result = getValue heightmap (5,4)
+
+    result |> should equal 6
+
+[<Fact>]
+let ``Get neighbors for item at top-left of heightmap``() =
+    let heightmap = readInput sampleInput
+    let result = getNeighbors heightmap (0,0)
+
+    let expected = [1;3]
+    result |> should equal expected
+
+[<Fact>]
+let ``Get neighbors for item at top-right of heightmap``() =
+    let heightmap = readInput sampleInput
+    let result = getNeighbors heightmap (9,0)
+
+    let expected = [1;1]
+    result |> should equal expected
+
+[<Fact>]
+let ``Get neighbors for item at bottom-right of heightmap``() =
+    let heightmap = readInput sampleInput
+    let result = getNeighbors heightmap (9,4)
+
+    let expected = [7;9]
+    result |> should equal expected
+
+[<Fact>]
+let ``Get neighbors for item at bottom-left of heightmap``() =
+    let heightmap = readInput sampleInput
+    let result = getNeighbors heightmap (0,4)
+
+    let expected = [8;8]
+    result |> should equal expected
+
