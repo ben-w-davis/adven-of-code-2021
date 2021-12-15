54f25fb47d46ca6287528cf333b4f2beb03a5c74
Ben Davis <benjamin.w.davis@gmail.com>
2021-12-15 13:01:26 -0600

    day 10 - parse a line

---
+++ AdventOfCode/SyntaxScoring.fs
@@ -0,0 +1,40 @@
+﻿module SyntaxScoring
+
+type SyntaxInput = {
+    Lines : string list
+}
+
+type SyntaxState = {
+    Stack : char list
+    SyntaxError : char option
+}
+let initSyntaxState = {
+    Stack = []
+    SyntaxError = None
+}
+
+let readInput (input:string) =
+    let lines =
+        input.Split("\r\n")
+        |> Array.toList
+    { Lines = lines }
+    
+let parseChar (c : char) state =
+    match (c, state.Stack) with
+    | '(',_ -> { state with Stack = c::state.Stack }
+    | ')','('::remainder -> { state with Stack = remainder }
+    | '[',_ -> { state with Stack = c::state.Stack }
+    | ']','['::remainder -> { state with Stack = remainder }
+    | '{',_ -> { state with Stack = c::state.Stack }
+    | '}','{'::remainder -> { state with Stack = remainder }
+    | '<',_ -> { state with Stack = c::state.Stack }
+    | '>','<'::remainder -> { state with Stack = remainder }
+    | _ -> { state with SyntaxError = Some c }
+
+let parseLine (line:string) =
+    let folder state c =
+        match state.SyntaxError with
+        | Some _ -> state
+        | None -> parseChar c state
+    line
+    |> Seq.fold folder initSyntaxState
---
+++ Test/Day10.fs
@@ -0,0 +1,97 @@
+﻿module Day10
+open System
+open Xunit
+open FsUnit.Xunit
+
+open SyntaxScoring
+
+let sampleInput = """[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
+
+[<Fact>]
+let ``Read first line``() =
+    let result = readInput sampleInput
+
+    result.Lines |> List.head |> should equal "[({(<(())[]>[[{[]{<()<>>"
+
+[<Fact>]
+let ``Parse open paren characters``() =
+    let result = parseChar '(' initSyntaxState
+    result.Stack |> should equal ['(']
+
+[<Fact>]
+let ``Parse open bracket characters``() =
+    let result = parseChar '[' initSyntaxState
+    result.Stack |> should equal ['[']
+
+[<Fact>]
+let ``Parse open curly characters``() =
+    let result = parseChar '{' initSyntaxState
+    result.Stack |> should equal ['{']
+
+[<Fact>]
+let ``Parse open angle characters``() =
+    let result = parseChar '<' initSyntaxState
+    result.Stack |> should equal ['<']
+
+[<Fact>]
+let ``Parse close paren characters``() =
+    let result = parseChar ')' { initSyntaxState with Stack = ['('] }
+    result.Stack |> should be Empty 
+
+[<Fact>]
+let ``Parse close bracket characters``() =
+    let result = parseChar ']' { initSyntaxState with Stack = ['['] }
+    result.Stack |> should be Empty
+
+[<Fact>]
+let ``Parse close curly characters``() =
+    let result = parseChar '}' { initSyntaxState with Stack = ['{'] }
+    result.Stack |> should be Empty
+
+[<Fact>]
+let ``Parse close angle characters``() =
+    let result = parseChar '>' { initSyntaxState with Stack = ['<'] }
+    result.Stack |> should be Empty
+
+[<Fact>]
+let ``Parse close paren characters as error``() =
+    let result = parseChar ')' initSyntaxState
+    result.SyntaxError |> should equal (Some ')')
+
+[<Fact>]
+let ``Parse close bracket characters as error``() =
+    let result = parseChar ']' initSyntaxState
+    result.SyntaxError |> should equal (Some ']')
+
+[<Fact>]
+let ``Parse close curly characters as error``() =
+    let result = parseChar '}' initSyntaxState
+    result.SyntaxError |> should equal (Some '}')
+
+[<Fact>]
+let ``Parse close angle characters as error``() =
+    let result = parseChar '>' initSyntaxState
+    result.SyntaxError |> should equal (Some '>')
+
+[<Fact>]
+let ``Parse first line of sample input``() =
+    let line = "[({(<(())[]>[[{[]{<()<>>"
+    let result = parseLine line
+
+    result.SyntaxError |> should equal None
+
+[<Fact>]
+let ``Parse corrupted line``() =
+    let line = "{([(<{}[<>[]}>{[]{[(<()>"
+    let result = parseLine line
+
+    result.SyntaxError |> should equal (Some '}')
